7f2f42f7264dfc16d8f28795150b6541d866a9df
MattyB709 <matthew.builes8@gmail.com>
2024-06-07 20:40:54 -0500

    Update mlutils.py docstring & format

--- mlutils.py
+++ mlutils.py
@@ -2,7 +2,13 @@ import torch
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-def train_epoch_classifcation(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, loss_fn: torch.nn.CrossEntropyLoss, optim: torch.optim.Optimizer) -> list[float]:
+def train_epoch_classifcation(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, optim: torch.optim.Optimizer, loss_fn: torch.nn.CrossEntropyLoss = torch.nn.CrossEntropyLoss()) -> list[float]:
+  '''
+  model: The model being trained
+  dataloader: The training dataloader
+  optim: The optimizer to use for training
+  loss_fn: (optional) the specific instance of nn.CrossEntropyLoss to use 
+  '''
   losses = []
   for (data, label) in iter(dataloader):
     logits = model(data)
